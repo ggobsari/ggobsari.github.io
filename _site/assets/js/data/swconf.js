3e5c8952ce887c3f83537804a42618c79c844fc5
ggobsari <ggobsari123@gmail.com>
2024-11-30 16:13:55 +0900

    추가 부수적인 변동사항 저장

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732950298',resources: [
+    cacheName: 'chirpy-1732950804',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
