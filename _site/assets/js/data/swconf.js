4c0870bba99319fb2d9bf3a3f59fd47448aec489
ggobsari <ggobsari123@gmail.com>
2025-01-08 20:26:42 +0900

    깃허브 블로그에 이미지 업로드 되는지 확인 커밋

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736270721',resources: [
+    cacheName: 'chirpy-1736335491',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
