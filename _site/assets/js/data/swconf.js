98a535f0dfa281a95fdd740e0e9ba54028b8e771
ggobsari <ggobsari123@gmail.com>
2025-01-10 03:31:51 +0900

    API, HTTP API, REST API 정리

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736433843',resources: [
+    cacheName: 'chirpy-1736447498',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
