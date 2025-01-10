4cb265c8d4d1a4d4400b0622baeeb6bf18485e4b
ggobsari <ggobsari123@gmail.com>
2025-01-10 18:11:27 +0900

    CSR과 SSR 정리

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736447498',resources: [
+    cacheName: 'chirpy-1736500266',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
